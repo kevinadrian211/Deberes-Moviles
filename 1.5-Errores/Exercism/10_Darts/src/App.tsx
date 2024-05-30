bea473ad9628bba95516a292cb656329ae2831e9
VisioTransit <kecvinadrian211@gmail.com>
2024-05-29 23:50:40 -0500

    Reac Router TC

--- 1.5-Errores/Exercism/10_Darts/src/App.tsx
+++ 1.5-Errores/Exercism/10_Darts/src/App.tsx
@@ -5,7 +5,7 @@ const App: React.FC = () => {
   return (
     <div>
       <h1>Score Calculator App</h1>
-      <ScoreCalculator x={1} y={1} />
+      <ScoreCalculator x={0} y={-1} />
       <ScoreCalculator x={8} y={6} />
       <ScoreCalculator x={2} y={2} />
     </div>
